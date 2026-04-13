e2706b44078f8c2972ee58c88acd8a653b26d293
ultraxincorporated-cmd <ultraxincorporated@gmail.com>
2026-04-13 03:23:20 -0400

    main.py

--- main.py
+++ main.py
@@ -4,19 +4,20 @@ import os
 WEBHOOK = os.environ["WEBHOOK"]
 GROUP_ID = 15938842
 
-url = f"https://catalog.roblox.com/v1/search/items?category=Clothing&creatorTargetId={GROUP_ID}&creatorType=Group&limit=10"
+url = f"https://catalog.roblox.com/v1/search/items/details?Category=3&CreatorType=2&CreatorTargetId={GROUP_ID}&Limit=30"
 
-res = requests.get(url, timeout=15).json()
+res = requests.get(url, timeout=15)
+data = res.json()
 
-for item in res.get("data", []):
+for item in data.get("data", []):
     item_id = item.get("id")
     name = item.get("name", "New item")
+    price = item.get("priceStatus")
 
-    price = item.get("price")
-    if price is None:
-        price = item.get("lowestPrice", 0)
+    if item_id and price == "Off Sale":
+        continue
 
-    if item_id and price and price > 0:
+    if item_id:
         link = f"https://www.roblox.com/catalog/{item_id}"
         requests.post(
             WEBHOOK,
